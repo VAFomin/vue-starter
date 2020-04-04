80e49b90424b12131b31a50e2363f8402c5ed9ff
Viktor <enotcode@gmail.com>
2020-04-04 22:08:36 +0300

    fix app bar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,23 @@
 <v-app>
     <v-app-bar app clipped-left>
       <v-toolbar-title>App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>Item 1</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="() => {}">
+            <v-list-item-title>Item 2</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-content>
       <v-container fluid>
