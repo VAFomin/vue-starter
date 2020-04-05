909cbdc069f0a37f283d8da1fbd6a78acedcd5dc
Viktor <enotcode@gmail.com>
2020-04-05 23:12:36 +0300

    add change theme

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,10 @@
     <v-app-bar app clipped-left>
       <v-toolbar-title>App</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu left bottom>
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
 
-        <v-list>
-          <v-list-item @click="() => {}">
-            <v-list-item-title>Item 1</v-list-item-title>
-          </v-list-item>
-          <v-list-item @click="() => {}">
-            <v-list-item-title>Item 2</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+      <v-btn icon @click="change_color()">
+        <v-icon>invert_colors</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container fluid>
@@ -27,3 +15,13 @@
     </v-content>
 </v-app>
 </template>
+
+<script>
+  export default{
+    methods: {
+      change_color () {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      },
+    }
+  }
+</script>
