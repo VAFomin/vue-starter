147cdee11f2c12a82e59452c139087cee30d7918
Viktor <enotcode@gmail.com>
2020-04-19 14:16:52 +0300

    add google Sign In button

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import router from "./plugins/router"
 import vuetify from './plugins/vuetify'
+import GSignInButton from 'vue-google-signin-button'
+
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(GSignInButton);
 
 new Vue({
-  router,
-  vuetify,
-  render: h => h(App),
+    router,
+    vuetify,
+    render: h => h(App),
 }).$mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="v-container pa-4">
-      <v-layout class="text-xs-center" column="column" justify-center="justify-center" align-center="align-center">
-          <v-flex xs12="xs12" md10="md10">
-              <div class="headline pb-4">
-                <h1>Home</h1>
-              </div>
-              <v-btn to="/page" x-large color="primary">Page</v-btn>
-          </v-flex>
-      </v-layout>
-  </div>
+    <div class="v-container pa-4">
+        <v-layout class="text-xs-center" column="column" justify-center="justify-center" align-center="align-center">
+            <v-flex xs12="xs12" md10="md10">
+                <div class="headline pb-4">
+                    <h1>Home</h1>
+                </div>
+                <g-signin-button
+                        :params="googleSignInParams"
+                        @success="onSignInSuccess"
+                        @error="onSignInError">
+                    Sign in with Google
+                </g-signin-button>
+                <v-btn to="/page" x-large color="primary">Page</v-btn>
+            </v-flex>
+        </v-layout>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                googleSignInParams: {
+                    clientId: '901060617777-1nic5bknq28bi7ohhj9k66398hlqtvp4.apps.googleusercontent.com'
+                }
+            }
+        },
+        methods: {
+            onSignInSuccess(googleUser) {
+                const profile = googleUser.getBasicProfile();
+                console.log(profile);
+                this.$router.replace("page");
+            },
+            onSignInError(error) {
+                console.log('OH NOES', error)
+            }
+        }
+    }
+</script>
+
+<style>
+    .g-signin-button {
+        display: inline-block;
+        padding: 4px 8px;
+        border-radius: 3px;
+        background-color: #3c82f7;
+        color: #fff;
+        box-shadow: 0 3px 0 #0f69ff;
+    }
+</style>
