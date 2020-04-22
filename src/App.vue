cd1e179cd45045a45676f9b728c4187286a2e534
Viktor <enotcode@gmail.com>
2020-04-22 08:43:54 +0300

    feature: new style by changing the theme

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,46 @@
 <template>
-<v-app>
-    <v-app-bar app clipped-left>
-      <v-toolbar-title>App</v-toolbar-title>
-      <v-spacer></v-spacer>
+    <v-app>
+        <v-app-bar app clipped-left>
+            <v-toolbar-title>App</v-toolbar-title>
+            <v-spacer></v-spacer>
 
-      <v-btn icon @click="change_color()">
-        <v-icon>invert_colors</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <v-footer app>
-      <span>Created by <a href="https://enotcode.com" target="_blank">enotcode</a></span>
-    </v-footer>
-</v-app>
+            <v-btn icon @click="change_color()">
+                <v-icon>{{ icon }}</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-content>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+        <v-footer app>
+            <span>Created by <a href="https://enotcode.com" target="_blank">enotcode</a></span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-  export default{
-    mounted() {
-      const theme = localStorage.getItem("useDarkTheme");
-        if (theme) {
-          if (theme == "true") {
-            this.$vuetify.theme.dark = true;
-          } else this.$vuetify.theme.dark = false;
-      }
-    },
-    methods: {
-      change_color () {
-        this.$vuetify.theme.dark= !this.$vuetify.theme.dark;
-        localStorage.setItem("useDarkTheme", this.$vuetify.theme.dark.toString())      
-      },
-    },
-  }
+    export default {
+        computed: {
+            icon: function () {
+                if (this.$vuetify.theme.dark) {
+                    return "wb_sunny"
+                } else {
+                    return "brightness_2"
+                }
+            }
+        },
+        mounted: function () {
+            const theme = localStorage.getItem("useDarkTheme");
+            if (theme) {
+                this.$vuetify.theme.dark = theme === "true";
+            }
+        },
+        methods: {
+            change_color() {
+                this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+                localStorage.setItem("useDarkTheme", this.$vuetify.theme.dark.toString())
+            },
+        },
+    }
 </script>
